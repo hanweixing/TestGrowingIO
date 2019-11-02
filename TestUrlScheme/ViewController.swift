4e12482197ecb3f3f13b190e8f77822462817b0e
hanweixing <weixing.han@tapatalk.com>
2019-11-02 22:54:10 +0800

    Initial Commit

---
+++ TestUrlScheme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestUrlScheme
+//
+//  Created by 韩卫星 on 2019/11/2.
+//  Copyright © 2019 韩卫星. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
